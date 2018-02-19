1e10d72abc4eddd2ec937a9d0d1347d4a6de6eee
Paweł Grabiński <pawelrgrabinski@gmail.com>
2018-02-20 00:13:34 +0100

    gitignorechanged3

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDqqVc_LkTk04v4Z0u-H1115zmGKL9ZqEg',
+    authDomain: 'morethanthreewishes-5b697.firebaseapp.com',
+    databaseURL: 'https://morethanthreewishes-5b697.firebaseio.com',
+    projectId: 'morethanthreewishes-5b697',
+    storageBucket: 'morethanthreewishes-5b697.appspot.com',
+    messagingSenderId: '433631247913'
+  }
+};
